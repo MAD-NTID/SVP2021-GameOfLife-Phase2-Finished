598e84cfbebbb3d0871fcc25a39526239bcd5722
cclcr21 <cclcr21@rit.edu>
2021-08-19 19:26:27 -0400

    + the last sections and their mappings

--- GameOfLifeDesktop/MyMainWindow.cs
+++ GameOfLifeDesktop/MyMainWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Controls; // 3.3
+using GameOfLife; // 4.1
 using GameOfLifeDesktop.UILibrary; // 2.3
 
 namespace GameOfLifeDesktop
@@ -32,5 +33,45 @@ namespace GameOfLifeDesktop
             mainGrid.Children.Add(cycleGrid); // 3.7
             Game.Start(); // 3.7
         } // 3.1        
+
+        protected override void OnNextCycle(Status[,] nextCycle) // 4.1
+        { // 4.1
+            for (int row = 0; row < Game.Rows; row++) // 4.2
+            { // 4.2
+                for (int column = 0; column < Game.Columns; column++) // 4.3
+                { // 4.3
+                    Status dataCell = nextCycle[row, column]; // 4.4
+                    UpdateWindow(() => // 4.5
+                    { // 4.5
+                        Image imgCell = GetCellByRowAndColumn(row, column); // 4.6
+                        if (dataCell == Status.Alive) // 4.6
+                            imgCell.Source = SelectedInstructorImage; // 4.6
+                        else // 4.6
+                            imgCell.Source = null;  // 4.6
+                    }); // 4.5
+                } // 4.3
+            } // 4.2
+
+            UpdateCycleStatistics(); // 4.7
+        } // 4.1
+
+        protected override void OnStopSimulation() // 5.1
+        { // 5.1
+            Game.Stop(); // 5.2
+        } // 5.1
+
+        protected override void OnResumeSimulation() // 6.1
+        { // 6.1
+            Game.Resume(); // 6.2
+        } // 6.1
+
+        protected override void OnResetSimulation() // 7.1
+        { // 7.1
+            Game = new GameOfLifeSession(); // 7.2
+            mainGrid.Children.Remove(cycleGrid); // 7.2
+
+            ClearUserInput(); // 7.3
+            ToggleInputEnabled(true); // 7.3
+        } // 7.1
     }
 }
